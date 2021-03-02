211b25aa77368c0dfb640a2f739400468cf6af3c
ese12880 <70610771+ese12880@users.noreply.github.com>
2021-03-02 10:40:41 -0600

    commit

--- static/js/app.js
+++ static/js/app.js
@@ -95,4 +95,78 @@ function optionChanged(val) {
           color:allOutIds
         }
       };
+     // Create the data array for our plot
+     var data_bubble = [trace2];
+     //  Create layout
+      var layout = {
+       xaxis: { title: "OTU Ids" },
+        showlegend: false,
+        height: 600,
+        width: 1000
+        
+      };
+     //  plotting the Bubble plot
+      Plotly.newPlot('bubble', data_bubble, layout);
+    // Gauge and Needle creation
+   //  Creating the level for needle
+    var level = 180*avgWfreq/9;
     
+    // Trig to calc meter point
+    var degrees = 180 - level,
+         radius = .5;
+    var radians = degrees * Math.PI / 180;
+    var x = radius * Math.cos(radians);
+    var y = radius * Math.sin(radians);
+    var path1 = (degrees < 45 || degrees > 135) ? 'M -0.0 -0.025 L 0.0 0.025 L ' : 'M -0.025 -0.0 L 0.025 0.0 L ';
+    // Path: may have to change to create a better triangle
+    var mainPath = path1,
+         pathX = String(x),
+         space = ' ',
+         pathY = String(y),
+         pathEnd = ' Z';
+    var path = mainPath.concat(pathX,space,pathY,pathEnd);
+   //  Creating the gauge
+    var data = [{ type: 'scatter',
+       x: [0], y:[0],
+        marker: {size: 14, color:'850000'},
+        showlegend: false,
+        name: 'average Srub',
+        text: avgWfreq,
+        hoverinfo: 'text+name'},
+      { values: [81/9, 81/9, 81/9, 81/9, 81/9, 81/9, 81/9, 81/9, 81/9, 81],
+      rotation: 90,
+      text: ['0-1','1-2','2-3','3-4','4-5','5-6','6-7','7-8','8-9',''],
+      direction: 'clockwise',
+      textinfo: 'text',
+      textposition:'inside',
+      marker: {colors: ['rgba(250, 235,220, 0.3)','rgba(255, 220,200, 0.5)','rgba(250, 210, 100, .5)','rgba(240, 200, 142, .5)','rgba(202, 190, 95, .2)','rgba(210, 180, 145, .5)','rgba(180, 170, 102, .5)','rgba(140, 160, 40, .5)','rgba(100, 106, 22, .5)','white']},
+      hoverinfo: 'label',
+      hole: .5,
+      type: 'pie',
+      showlegend: false
+    }];
+    
+    var layout = {
+      shapes:[{
+          type: 'path',
+          path: path,
+          fillcolor: '850000',
+          line: {
+            color: '850000'
+          }
+        }],
+      height: 500,
+      width: 500,
+      xaxis: {zeroline:false, showticklabels:false,
+                 showgrid: false, range: [-1, 1]},
+      yaxis: {zeroline:false, showticklabels:false,
+                 showgrid: false, range: [-1, 1]},
+     title:"<b>Belly Button Washing Frequency</b><br>Scrubs per week"
+ 
+ 
+    };
+   //  Plotting the gauge plot
+    Plotly.newPlot('gauge', data, layout);
+    });}
+ // Calling the Main function when page load
+    main(940);  
